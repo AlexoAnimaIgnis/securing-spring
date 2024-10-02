887121ed3aa4b0a4cdd9ba42425af2b784acc6d4
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-10-02 14:26:37 +0800

    initial commit

---
+++ src/main/java/com/alexo/demo/springsecuringapp/SpringSecuringAppApplication.java
@@ -0,0 +1,13 @@
+package com.alexo.demo.springsecuringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuringAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexo/demo/springsecuringapp/SpringSecuringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexo.demo.springsecuringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
